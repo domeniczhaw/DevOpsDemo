b7ea41719ac83ea30c80c4dfd35662b9c0ecfb36
domeniclargiader <domenic.largiader@gmail.com>
2023-03-27 23:06:17 +0200

    Anpassung Applikationsfile & ern. Ausführung Tests

--- backend/src/main/java/ch/zhaw/iwi/devops/heatrecommendation/heatrecommendation.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/heatrecommendation/heatrecommendation.java
@@ -3,8 +3,12 @@ package ch.zhaw.iwi.devops.heatrecommendation;
 public class heatrecommendation {
 
 	static String recommendationheat1(int nummer){
-        return "1";
-    }
+            if (nummer<=12){
+                return "Du solltest eine dicke Jacke anziehen";
+            }
+            return String.valueOf(nummer);
+    
+        }   
 }
 
 
