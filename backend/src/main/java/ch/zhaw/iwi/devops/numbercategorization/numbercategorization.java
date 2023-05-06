9cfac129aa7943e618ac0e2cdcb1eed62a097370
domeniclargiader <domenic.largiader@gmail.com>
2023-05-06 18:08:06 +0200

    Adjusting code smell based on Sonar Report

--- backend/src/main/java/ch/zhaw/iwi/devops/numbercategorization/numbercategorization.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/numbercategorization/numbercategorization.java
@@ -1,7 +1,12 @@
-package ch.zhaw.iwi.devops.numbercategorization;
-import 
+package ch.zhaw.iwi.devops.numberCategorization;
+ 
 
-public class numbercategorization {
+class numberCategorization {
+
+    private numberCategorization() {
+        throw new IllegalStateException("Utility class");
+    }
+    
     public static int getCategory(int number) {
         int category;
         if (number < 0) {
@@ -11,11 +16,10 @@ public class numbercategorization {
         } else if (number > 10 && number <= 100) {
             category = 2;
         } else {
-            category = 3
+            category = 3;
         }
         return category;
     }
-    k
-}
-}
     
+}
+
